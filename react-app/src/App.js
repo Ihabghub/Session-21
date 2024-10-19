c21ddf460eb7a43cdd534f9027211c57ddda0e6d
Ihab <ihabsafa13@gmail.com>
2024-10-19 22:37:25 +0300

    added

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,32 +1,24 @@
-import React from "react";
+import "./App.css";
+import Header from "./Header";
+import Footer from "./Footer";
+import Booking from "./Booking";
+import { Routes, Route } from "react-router-dom";
+import Reservation from "./Reservation";
+import Home from "./Home";
+import Payment from "./Payment"; // Import Payment component
 
 function App() {
   return (
-    <div className="bg-black">
-      <header className="flex flex-row bg-[#EACE91] w-full justify-between p-7">
-        <div className="text-2xl font-bold">
-          <h1>BarberSHOP!</h1>
-        </div>
-        <div className="flex justify-between w-1/4 text-lg">
-          <p>Insta</p>
-          <p>Facebook</p>
-          <p>Twitter</p>
-        </div>
-      </header >
-      <div className="flex">
-        <div className="mt-10 ml-8">
-          <img className="w-3000 h-6000" src="./Frame 2.png"></img>
-        </div>
-        <div>
-          <div className="text-[#EACE91]">
-            <h1>Welcome TO B&H's BarberShop!</h1>
-          </div>
-          <div className="text-[#EACE91]">
-            <button>Book NOW!</button>
-          </div>
-        </div>
-      </div>
-    </div >
+    <div className="App">
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/booking" element={<Booking />} />
+        <Route path="/reservation" element={<Reservation />} />
+        <Route path="/payment" element={<Payment />} /> {/* Add Payment route */}
+      </Routes>
+      <Footer />
+    </div>
   );
 }
 
